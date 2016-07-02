f2cb71e25a2bdda55d2c4d7d896ab3406a1f8928
hbrannan <hannahbrannan@gmail.com>
2016-07-01 21:56:59 -0700

    Add psuedocode plan for controllers (dataflow: app>routes>contr>models>schema)

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,13 +3,25 @@ var models = require('../models');
 module.exports = {
   messages: {
     get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    post: function (req, res) {
+      //intercept the posts here and send the body to the models
+        //mpodels will then send that body to the controllers. 
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
     get: function (req, res) {},
-    post: function (req, res) {}
+    post: function (req, res) {
+      //intercept the posts here and send the body to the models
+            //make sure that you .toSTring or JSON.stsringify to nterpret data
+        //mpodels will then send that body to the controllers. 
+    // a function which handles posting a message to the database
+
+    //on end of the request, you prbs need to rwrite head 
+    //and end the response 
+
+    }
   }
 };
 
